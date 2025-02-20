a8fb9eea0192c5a70b265fce63042cfc96fd58b4
ReddikHaien <584969@stud.hvl.no>
2025-02-20 16:48:21 +0100

    compression and moved into folder

--- publish-proj.sh
+++ publish-proj.sh
@@ -6,7 +6,9 @@ dotnet build $2 -c Release --no-restore -p Version=$1
 
 dotnet publish $2 -c Release --output $3
 
-(cd $3 && rm *.pdb)
-
-zip -rv build-output.zip $3
-tar -cvf build-output.tar $3
+(
+    cd $3
+    && rm *.pdb
+    && zip -rv ../build-output.zip .
+    && tar -czvf build-output.tar.gz .
+)
