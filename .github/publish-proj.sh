5d5e846c0c045f35449aca20a494d39f5840c432
ReddikHaien <584969@stud.hvl.no>
2025-02-20 01:31:01 +0100

    more snacks

---
+++ .github/publish-proj.sh
@@ -0,0 +1,10 @@
+$version = $1
+$project= $2
+$output = $3
+
+
+dotnet restore ../../$project
+
+dotnet build ../../$project -c Release --no-restore -p Version=$version
+
+dotnet publish ../../$project -c Release --output $output
--- .github/workflows/publish-proj.sh
+++
@@ -1,9 +0,0 @@
-$version = $1
-$project= $2
-$output = $3
-
-dotnet restore ../../../$project
-
-dotnet build ../../../$project -c Release --no-restore -p Version=$version
-
-dotnet publish ../../../$project -c Release --output $output
