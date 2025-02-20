72a1e71c5e12f2742b81380cdc70ccddbcd2bfe6
ReddikHaien <584969@stud.hvl.no>
2025-02-20 18:58:05 +0100

    fix(component): correct message format

--- src/SheetCuttingTools.Grasshopper/Components/Converters/MeshConverterComponent.cs
+++ src/SheetCuttingTools.Grasshopper/Components/Converters/MeshConverterComponent.cs
@@ -24,7 +24,7 @@ namespace SheetCuttingTools.Grasshopper.Components.Converters
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddMeshParameter("Mesh", "M", "The produced mesh", GH_ParamAccess.item);
+            pManager.AddMeshParameter("Mesh", "M", "The produced mesh.", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
