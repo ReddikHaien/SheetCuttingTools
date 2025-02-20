f2819d81d9bd5589b4c1cc3c393e3640b9d42ca0
ReddikHaien <584969@stud.hvl.no>
2025-02-20 16:56:05 +0100

    maybe this will work

--- publish-proj.sh
+++ publish-proj.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-dotnet restore $2
+# dotnet restore $2
 
-dotnet build $2 -c Release --no-restore -p Version=$1
+# dotnet build $2 -c Release --no-restore -p Version=$1
 
 dotnet publish $2 -c Release --output $3
 
-(
-    cd $3
-    && rm *.pdb
-    && zip -rv ../build-output.zip .
-    && tar -czvf build-output.tar.gz .
-)
+(cd $3 \
+    && rm *.pdb \
+    && zip -rv ../build-output.zip . \
+    && tar -czvf build-output.tar.gz .)
--- verify-proj.sh
+++ verify-proj.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(dotnet restore $1 3>&2 2>&1 1>&3) 2>/dev/null
-(dotnet build $1 -c Release --no-restore 3>&2 2>&1 1>&3) 2>/dev/null
+(dotnet restore $2 3>&2 2>&1 1>&3) 2>/dev/null
+(dotnet build $2 -c Release --no-restore -p Version=$1 3>&2 2>&1 1>&3) 2>/dev/null
