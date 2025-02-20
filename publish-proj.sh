850fa37e74d085730ff026e9f2097062c58aeeb0
ReddikHaien <584969@stud.hvl.no>
2025-02-20 16:21:09 +0100

    zip and tar production

--- publish-proj.sh
+++ publish-proj.sh
@@ -5,3 +5,8 @@ dotnet restore $2
 dotnet build $2 -c Release --no-restore -p Version=$1
 
 dotnet publish $2 -c Release --output $3
+
+(cd $3 && rm *.pdb)
+
+zip build-output.zip $3
+tar -cf build-output.tar $3
